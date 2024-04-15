f68b9ab8c2dc488eb66bd9f3da9cc056d0546f54
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-15 15:13:53 +0300

    aritmetik işlemler

--- elazig.sql
+++ elazig.sql
@@ -1,7 +1,18 @@
 -- Single Comment
+/* 
+Multiple Comment
+*/
 
 -- Database Seç
 use nortwind;
 
 -- Select 
 select * from Categories;
+
+-- Aritmetic Operators
+-- + - * / %
+select 9+3 as toplam;
+select 9-3 as kalan;
+select 9*3 as çarpma;
+select 9/3 as bölüm;
+select 9%3 as "Bölümünden kalan";
