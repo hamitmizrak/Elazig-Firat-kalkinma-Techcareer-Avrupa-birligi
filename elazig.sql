b4b4a033d51993db3dbdb4fdb38efa68da3fc25b
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-15 15:24:41 +0300

    select

--- elazig.sql
+++ elazig.sql
@@ -11,8 +11,25 @@ select * from Categories;
 
 -- Aritmetic Operators
 -- + - * / %
+-- as: alias (Takma Ad) kýsaltmak için kullanýyoruz.
 select 9+3 as toplam;
 select 9-3 as kalan;
 select 9*3 as çarpma;
-select 9/3 as bölüm;
+select 9/3 bölüm;
 select 9%3 as "Bölümünden kalan";
+
+------------------------------------------------------------
+------------------------------------------------------------
+-- DQL (Data Query Language)
+use nortwind;
+select * from Categories;
+
+-- Ýstediðim Sutunu çaðýrmak için kullanýyorum.
+select CategoryID,CategoryName from Categories;
+
+select cat.CategoryID,cat.CategoryName from Categories as cat;
+
+
+
+-- Bu sorguda direk database adý ile çaðýrýyorum.
+select * from [nortwind].[dbo].[Categories] ;
