a828199bccc49de9d360154240d9aa28ad7388c0
Hamit Mizrak <hamitmizrak@gmail.com>
2024-04-15 15:41:23 +0300

    DQL, Aggreagate count

--- elazig.sql
+++ elazig.sql
@@ -21,6 +21,8 @@ select 9%3 as "Bölümünden kalan";
 ------------------------------------------------------------
 ------------------------------------------------------------
 -- DQL (Data Query Language)
+-- select kolonAdi from tabloAdi 
+
 use nortwind;
 select * from Categories;
 
@@ -29,7 +31,18 @@ select CategoryID,CategoryName from Categories;
 
 select cat.CategoryID,cat.CategoryName from Categories as cat;
 
-
-
 -- Bu sorguda direk database adý ile çaðýrýyorum.
 select * from [nortwind].[dbo].[Categories] ;
+
+-- Nortwind databasesinden Categories tablosundaki ID>=5 yukarýnda olanlarý listeleyelim?
+-- select kolonAdi from tabloAdi 
+-- where: þart 
+select * from [nortwind].[dbo].[Categories] as cat where cat.categoryID>=5;
+
+-- Aggreagate
+
+-- Count: Sayma iþlemlerinden kullanýyorsunuz.
+-- Nortwind databasesinden Categories tablosundaki ID>=5 yukarýnda olanlarý kaç tane ?
+select count(*) as toplamAdedi from [nortwind].[dbo].[Categories] as cat where cat.categoryID>=5;
+
+-
